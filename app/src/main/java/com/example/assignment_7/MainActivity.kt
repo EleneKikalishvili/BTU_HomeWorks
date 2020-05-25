fc0c7141544c42f3b16ef155d416765b8240542f
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-25 04:05:38 +0400

    assignment done

--- app/src/main/java/com/example/assignment_7/MainActivity.kt
+++ app/src/main/java/com/example/assignment_7/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.assignment_7
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Global.getString
+import android.system.Os.bind
+import android.text.TextUtils
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.note_item.*
 
 class MainActivity : AppCompatActivity() {
+    var notes: MutableSet<String> = mutableSetOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myAdapter = NoteListAdapter(this, notes.toMutableList())
+
+        val sharedPreferences=getSharedPreferences("MyApplication", Context.MODE_PRIVATE)
+        val savedText =  sharedPreferences.getStringSet("NOTE", setOf<String>())
+        if(savedText != null){
+            notes = savedText.toMutableSet()
+        }
+
+        addBtn.setOnClickListener {
+
+            val input = inputText.text.toString()
+
+            if (!TextUtils.isEmpty(input)) {
+                notes.add(input)
+                myAdapter.add(input)
+                myAdapter.notifyDataSetChanged()
+                inputText.setText("")
+               sharedPreferences.edit().putStringSet("NOTE", notes).apply()
+            }
+        }
+
+        recyclerView.adapter = myAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
